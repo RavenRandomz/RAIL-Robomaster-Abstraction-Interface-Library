575101e94d39deac6de9db366cf05318c0443cef
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-01-09 14:13:57 -0600

    Add IWattageBudgeter
    Defines the interface for anything that budgets.

---
+++ include/iwattagebudgeter.h
@@ -0,0 +1,9 @@
+#ifndef IWATTAGE_BUDGETERLKASDFJNVJLKVWA
+#define IWATTAGE_BUDGETERLKASDFJNVJLKVWA
+class IWattageBudgeter
+{
+public:
+	virtual void setBudget(double wattage) = 0;
+	virtual double getBudget() const = 0;
+};
+#endif
