1fd725d5ec1e98be0958c392930078a8d8e169cb
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-01-09 15:17:55 -0600

    Add IBudgeterList

---
+++ include/ibudgeterlist.h
@@ -0,0 +1,26 @@
+#ifndef IBUDGETERLIST_H_SLIEJFSEFILJXXNCSE
+#define IBUDGETERLIST_H_SLIEJFSEFILJXXNCSE
+
+#include "wattagealiases.h"
+
+namespace rail
+{
+	class BudgeterList
+	{
+	private:
+		BudgeterVector_t m_budgeters {};
+
+	public:
+		/// If a budgeter is already in the list it is ignored)
+		/**
+		 * This is determined by memory address.
+		 */
+		void addBudgeter(BudgeterPointer_t budgeter);
+		void removeBudgeter(BudgeterPointer_t budgeter);
+
+		/// Return the number of budgeters
+		int getSize();
+		BudgeterVector_t getBudgeters();
+	};
+}
+#endif
