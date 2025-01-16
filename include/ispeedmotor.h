abb0a1ddb9fb8bb01ec8ae4634da37497a56755a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-01-16 15:01:50 -0600

    Make speed motor a budgeter

--- include/ispeedmotor.h
+++ include/ispeedmotor.h
@@ -1,10 +1,13 @@
 #ifndef ISPEEDMOTOR_H_SDFLKJSFLKEJFZXCSEF
 #define ISPEEDMOTOR_H_SDFLKJSFLKEJFZXCSEF
+#include "ibudgeter.h"
 namespace rail
 {
-	class ISpeedMotor
+	class ISpeedMotor : IBudgeter
 	{
 	public:
+		virtual void setBudget(double wattage) override = 0;
+		virtual double getBudget() const override = 0;
 		virtual void setSpeed(double speed) = 0;
 		virtual double getSpeed() const = 0;
 	};
