ecd62eaabae3032f754afb2589c372eaddad04f7
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-01-16 14:56:39 -0600

    Add TorqueMotor

---
+++ include/itorquemotor.h
@@ -0,0 +1,12 @@
+#ifndef ISPEEDMOTOR_H_SDFLKJSFLKEJFZXCSEF
+#define ISPEEDMOTOR_H_SDFLKJSFLKEJFZXCSEF
+namespace rail
+{
+	class ITorqueMotor
+	{
+	public:
+		virtual void setTorque(double speed) = 0;
+		virtual double getTorque() const = 0;
+	};
+}
+#endif
