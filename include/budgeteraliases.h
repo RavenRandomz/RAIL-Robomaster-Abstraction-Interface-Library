f7a314903ec1aa6e6e9470be809c7d1992901d0c
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-01-16 14:53:55 -0600

    Change wattagealises to bdugeteralises

--- include/budgeteraliases.h
+++ include/budgeteraliases.h
@@ -1,12 +1,12 @@
 #ifndef WATTAGEALIASES_H_SLDKFASDFSEFX
 #define WATTAGEALIASES_H_SLDKFASDFSEFX
-#include "iwattagebudgeter.h"
+#include "ibudgeter.h"
 
 #include <memory>
 #include <vector>
 namespace rail
 {
-	using BudgeterPointer_t = std::shared_ptr<rail::IWattageBudgeter>;
+	using BudgeterPointer_t = std::shared_ptr<rail::IBudgeter>;
 	using BudgeterVector_t = std::vector<BudgeterPointer_t>;
 }
 #endif
