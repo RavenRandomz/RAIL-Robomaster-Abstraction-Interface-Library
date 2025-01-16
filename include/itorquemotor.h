92a7bf78a62e34af0a04c5eeab78fbf7b092ae50
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-01-16 15:02:49 -0600

    Make torquemotor a budgeter

--- include/itorquemotor.h
+++ include/itorquemotor.h
@@ -1,10 +1,14 @@
 #ifndef ITORQUEMOTOR_H_SDFLKJSFLKEJFZXCSEF
 #define ITORQUEMOTOR_H_SDFLKJSFLKEJFZXCSEF
+#include "ibudgeter.h"
+
 namespace rail
 {
-	class ITorqueMotor
+	class ITorqueMotor : IBudgeter
 	{
 	public:
+		virtual void setBudget(double wattage) override = 0;
+		virtual double getBudget() const override = 0;
 		virtual void setTorque(double speed) = 0;
 		virtual double getTorque() const = 0;
 	};
