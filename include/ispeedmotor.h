1b8d1e55f8595609bf1636bda86c055c65a3becd
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-09 16:24:56 -0500

    Add speed motor max and min info

--- include/ispeedmotor.h
+++ include/ispeedmotor.h
@@ -8,8 +8,13 @@ namespace rail
 	public:
 		virtual void setBudget(double wattage) override = 0;
 		virtual double getBudget() const override = 0;
+
 		virtual void setSpeed(double speed) = 0;
 		virtual double getSpeed() const = 0;
+		virtual void setMaxSpeed(double maxSpeed) = 0;
+		virtual double getMaxSpeed() const = 0;
+		virtual void setMinSpeed(double maxSpeed) = 0;
+		virtual double getMinSpeed() const = 0;
 	};
 }
 #endif
