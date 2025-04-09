bc784ebcfde432a42adfe4f133322281063c384c
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-09 16:28:44 -0500

    Update DC motor to have min and max

--- include/idcmotor.h
+++ include/idcmotor.h
@@ -11,10 +11,21 @@ namespace rail
 
 		virtual void setBudget(double wattage) override = 0;
 		virtual double getBudget() const override = 0;
+
+		virtual void setSpeed(double speed) override = 0;
+		virtual double getSpeed() const override = 0;
+		virtual void setMaxSpeed(double maxSpeed) override = 0;
+		virtual double getMaxSpeed() const override = 0;
+		virtual void setMinSpeed(double maxSpeed) override = 0;
+		virtual double getMinSpeed() const override = 0;
+
 		virtual void setTorque(double speed) override = 0;
 		virtual double getTorque() const override = 0;
-		virtual void setSpeed(double speed) override= 0;
-		virtual double getSpeed() const override= 0;
+		virtual void setMaxTorque(double maxSpeed) override = 0;
+		virtual double getMaxTorque() const override = 0;
+		virtual void setMinTorque(double maxSpeed) override = 0;
+		virtual double getMinTorque() const override = 0;
+
 	};
 }
 #endif
