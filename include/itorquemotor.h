b5095a72d1abae89a5ac10973e22d6b3f1fe1d4f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-09 16:25:41 -0500

    Add max torque and min torque functions

--- include/itorquemotor.h
+++ include/itorquemotor.h
@@ -9,8 +9,13 @@ namespace rail
 	public:
 		virtual void setBudget(double wattage) override = 0;
 		virtual double getBudget() const override = 0;
+
 		virtual void setTorque(double speed) = 0;
 		virtual double getTorque() const = 0;
+		virtual void setMaxTorque(double maxSpeed) = 0;
+		virtual double getMaxTorque() const = 0;
+		virtual void setMinTorque(double maxSpeed) = 0;
+		virtual double getMinTorque() const = 0;
 	};
 }
 #endif
