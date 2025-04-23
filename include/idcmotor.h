c2c4269bf2560a8e13b9c55c52c6f710e484f8d5
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-23 13:14:36 -0500

    Templatize IDCMotor and make improvements
    Functions need to be updated. Making arbitrary types be passed by const reference in order to ensure efficiency and preventing of
    'side effects'

--- include/idcmotor.h
+++ include/idcmotor.h
@@ -6,25 +6,28 @@
 
 namespace rail
 {
-	class IDCMotor : ITorqueMotor, ISpeedMotor 
+	template <typename SpeedData, typename TorqueData, typename WattData>
+	class IDCMotor : public ITorqueMotor<TorqueData, WattData>, public ISpeedMotor<SpeedData, WattData>
 	{
 
-		virtual void setBudget(double wattage) override = 0;
-		virtual double getBudget() const override = 0;
+		virtual void setBudget(const WattData& wattage) override = 0;
+		virtual const WattData& getBudget() const override = 0;
 
-		virtual void setSpeed(double speed) override = 0;
-		virtual double getSpeed() const override = 0;
-		virtual void setMaxSpeed(double maxSpeed) override = 0;
-		virtual double getMaxSpeed() const override = 0;
-		virtual void setMinSpeed(double maxSpeed) override = 0;
-		virtual double getMinSpeed() const override = 0;
+		virtual void setSpeed(const SpeedData& speed) override = 0;
+		virtual const SpeedData& getSpeed() const override = 0;
 
-		virtual void setTorque(double speed) override = 0;
-		virtual double getTorque() const override = 0;
-		virtual void setMaxTorque(double maxSpeed) override = 0;
-		virtual double getMaxTorque() const override = 0;
-		virtual void setMinTorque(double maxSpeed) override = 0;
-		virtual double getMinTorque() const override = 0;
+		virtual void setMaxSpeed(const SpeedData& maxSpeed) override = 0;
+		virtual const SpeedData& getMaxSpeed() const override = 0;
+		virtual void setMinSpeed(const SpeedData& maxSpeed) override = 0;
+		virtual const SpeedData& getMinSpeed() const override = 0;
+
+		virtual void setTorque(const TorqueData& speed) override = 0;
+		virtual const TorqueData& getTorque() const override = 0;
+
+		virtual void setMaxTorque(const TorqueData& maxSpeed) override = 0;
+		virtual const TorqueData& getMaxTorque() const override = 0;
+		virtual void setMinTorque(const TorqueData& maxSpeed) override = 0;
+		virtual const TorqueData& getMinTorque() const override = 0;
 
 	};
 }
