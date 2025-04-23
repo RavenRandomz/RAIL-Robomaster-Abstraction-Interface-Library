a6082f82288b2d854a14d60c2942b232a497e45d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-23 12:55:02 -0500

    Make IPositionMotor a template

--- include/ipositionmotor.h
+++ include/ipositionmotor.h
@@ -4,13 +4,21 @@
 
 namespace rail
 {
-	class IPositionMotor : IWattBudgeter
+	template <typename PositionData, typename WattData>
+	class IPositionMotor : public IWattBudgeter<WattData>
 	{
 	public:
 		virtual void setBudget(double wattage) override = 0;
 		virtual double getBudget() const override = 0;
+
 		virtual void setPosition(double speed) = 0;
 		virtual double getPosition() const = 0;
+
+		virtual void setMaxPosition(PositionData position) = 0;
+		virtual PositionData getMaxPosition() const = 0;
+
+		virtual void setMinPosition(PositionData position) = 0;
+		virtual PositionData getMinPosition() const = 0;
 	};
 }
 #endif
