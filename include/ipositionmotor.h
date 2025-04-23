64fd4e17c635a8573e78636de6069620a29fa6aa
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-23 15:03:13 -0500

    Make positionmotor use const references

--- include/ipositionmotor.h
+++ include/ipositionmotor.h
@@ -8,17 +8,17 @@ namespace rail
 	class IPositionMotor : public IWattBudgeter<WattData>
 	{
 	public:
-		virtual void setBudget(double wattage) override = 0;
-		virtual double getBudget() const override = 0;
+		virtual void setBudget(const WattData& wattage) override = 0;
+		virtual const WattData& getBudget() const override = 0;
 
-		virtual void setPosition(double speed) = 0;
-		virtual double getPosition() const = 0;
+		virtual void setPosition(const PositionData& speed) = 0;
+		virtual const PositionData& getPosition() const = 0;
 
-		virtual void setMaxPosition(PositionData position) = 0;
-		virtual PositionData getMaxPosition() const = 0;
+		virtual void setMaxPosition(const PositionData& position) = 0;
+		virtual const PositionData& getMaxPosition() const = 0;
 
-		virtual void setMinPosition(PositionData position) = 0;
-		virtual PositionData getMinPosition() const = 0;
+		virtual void setMinPosition(const PositionData& position) = 0;
+		virtual const PositionData& getMinPosition() const = 0;
 	};
 }
 #endif
