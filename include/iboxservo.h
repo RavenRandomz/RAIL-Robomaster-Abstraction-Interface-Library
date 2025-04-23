c5affbb545b1c859676dec295bb3aa529b1669af
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-23 15:10:15 -0500

    Add Box Servo
    Basically a position motor but might need to have stuff added in the future that is only relevant to servos

---
+++ include/iboxservo.h
@@ -0,0 +1,25 @@
+#ifndef I_BOX_SERVO_ASFIJSSLEFIJSEIFJ9WEFW3OIRJ
+#define I_BOX_SERVO_ASFIJSSLEFIJSEIFJ9WEFW3OIRJ
+#include "ipositionmotor.h"
+#include "iwattbudgeter.h"
+namespace rail
+{
+	template <typename PositionData, typename WattData>
+	class IBoxServo : public IPositionMotor<PositionData, WattData>, public IWattBudgeter<WattData>
+	{
+	public:
+		virtual void setBudget(const WattData& wattage) override = 0;
+		virtual const WattData& getBudget() const override = 0;
+
+		virtual void setPosition(const PositionData& speed) = 0;
+		virtual const PositionData& getPosition() const = 0;
+
+		virtual void setMaxPosition(const PositionData& position) = 0;
+		virtual const PositionData& getMaxPosition() const = 0;
+
+		virtual void setMinPosition(const PositionData& position) = 0;
+		virtual const PositionData& getMinPosition() const = 0;
+	};
+}
+
+#endif
