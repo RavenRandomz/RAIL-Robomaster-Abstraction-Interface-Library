de7b45ee52ebaaa2207ccd9873db92da927d0259
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-29 10:07:18 -0500

    Add Overbudget functionality to IWattBudgeter

--- include/iwattbudgeter.h
+++ include/iwattbudgeter.h
@@ -8,6 +8,11 @@ namespace rail
 	public:
 		virtual void setWattBudget(const WattData& wattage) = 0;
 		virtual WattData getWattBudget() const = 0;
+
+		virtual bool getOverbudgetStatus() const = 0;
+		virtual void resetOverbudgetStatus() const = 0;
+		virtual WattData getOverBudgetAmount() const = 0;
+
 		virtual ~IWattBudgeter();
 	};
 }
