970ce4d9e04cb4b05bc7eec8b3a312edbcf4b7af
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-29 10:14:35 -0500

    Add wattage estimate and overbudget functions

--- include/iwattbudgeter.h
+++ include/iwattbudgeter.h
@@ -9,9 +9,18 @@ namespace rail
 		virtual void setWattBudget(const WattData& wattage) = 0;
 		virtual WattData getWattBudget() const = 0;
 
-		virtual bool getOverbudgetStatus() const = 0;
-		virtual void resetOverbudgetStatus() const = 0;
-		virtual WattData getOverBudgetAmount() const = 0;
+		/**
+		 * @brief This disables any physical action but allows the budgeter
+		 * to have any functions called that would impact its estimation
+		 * If it goes into overbudget, a WattManager can detect it before it acts
+		 * on the overbuget settings.
+		 *
+		 * The user has the option to not utilize EstimateMode for higher risk.
+		 */
+		virtual void setWattEstimateMode(bool mode) = 0;
+		virtual bool getWattOverbudgetStatus() const = 0;
+		virtual void resetWattOverbudgetStatus() = 0;
+		virtual WattData getWattOverbudgetAmount() const = 0;
 
 		virtual ~IWattBudgeter();
 	};
