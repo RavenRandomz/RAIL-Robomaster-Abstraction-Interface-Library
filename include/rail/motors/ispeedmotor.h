33e88b13a3560f4c4b83b61dc1da492ff0e3a4f8
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-02 15:09:40 -0500

    clean(ISpeedMotor): Remove unnecessary limiter functions

--- include/rail/motors/ispeedmotor.h
+++ include/rail/motors/ispeedmotor.h
@@ -9,12 +9,6 @@ namespace rail
 		virtual void setSpeed(const SpeedData& speed) = 0;
 		virtual SpeedData getSpeed() const = 0;
 
-		virtual void setMaxSpeed(const SpeedData& maxSpeed) = 0;
-		virtual SpeedData getMaxSpeed() const = 0;
-
-		virtual void setMinSpeed(const SpeedData& minSpeed) = 0;
-		virtual SpeedData getMinSpeed() const = 0;
-
 		virtual ~ISpeedMotor() = default;
 	};
 }
