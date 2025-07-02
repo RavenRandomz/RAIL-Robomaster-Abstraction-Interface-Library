32299c7b6fdfd2e74ec40c962b800bb8dd5b13e5
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-02 15:10:33 -0500

    clean(IPositionMotor): Remove bloat

--- include/rail/motors/ipositionmotor.h
+++ include/rail/motors/ipositionmotor.h
@@ -9,12 +9,6 @@ namespace rail
 		virtual void setPosition(const PositionData& speed) = 0;
 		virtual PositionData getPosition() const = 0;
 
-		virtual void setMaxPosition(const PositionData& maxPosition) = 0;
-		virtual PositionData getMaxPosition() const = 0;
-
-		virtual void setMinPosition(const PositionData& minPosition) = 0;
-		virtual PositionData getMinPosition() const = 0;
-
 		virtual ~IPositionMotor() = default;
 	};
 }
