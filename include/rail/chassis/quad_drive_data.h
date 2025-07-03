200e077aab14a00413b0eb93aa9b50516fbda02b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-03 17:14:35 -0500

    feat(QuadDriveData): Initialize

---
+++ include/rail/chassis/quad_drive_data.h
@@ -0,0 +1,21 @@
+#ifndef RAIL_CHASSIS_QUAD_DRIVE_DATA_H
+#define RAIL_CHASSIS_QUAD_DRIVE_DATA_H
+
+namespace rail::chassis
+{
+	/**
+	 *	Provides a means of bundling information such as speed, temperature, and whatnot
+	 *  involving a quad drive.
+	 *  This is a template in order to support the use of units libraries or primitive types
+	 */
+	template <typename Data>
+	class QuadDriveData
+	{
+		Data frontLeft;
+		Data frontRight;
+		Data rearLeft;
+		Data rearRight;
+	};
+}
+#endif
+
