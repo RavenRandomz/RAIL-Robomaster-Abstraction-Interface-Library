c5542cd83ce8e4b4e8a6950b9d4b94e8cc22075a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-03 18:06:19 -0500

    feat(IHolonomicDrive): Initialize

---
+++ include/rail/chassis/iholonomic_drive.hpp
@@ -0,0 +1,20 @@
+#ifndef RAIL_CHASSIS_HOLONOMIC_DRIVE_HPP
+#define RAIL_CHASSIS_HOLONOMIC_DRIVE_HPP
+namespace rail::chassis
+{
+    /**
+     * A Holonomic Drive is essentially any 2d drive that can translate
+     * in any direction without having to rotate. The most common ones are:
+     * mecanum, swerve, and omni 
+     * Read more here: https://gm0.org/en/latest/docs/common-mechanisms/drivetrains/holonomic.html
+     * It is recommended to have velocity in a class that can get
+     * x and y.
+     */
+    template <class Velocity, class AngularVelocity>
+    class IHolonomicDrive
+    {
+        virtual void setTargetTranslation(const Velocity& velocity) = 0;
+        virtual void setTargetRotation(const AngularVelocity& rotation) = 0;
+    };
+}
+#endif
