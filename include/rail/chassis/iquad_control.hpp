29200d2028027ed14509704de73e8a0841b6537f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 17:20:42 -0500

    feat(IQuadControl): Allow non-owning direct control of a quad drive

---
+++ include/rail/chassis/iquad_control.hpp
@@ -0,0 +1,21 @@
+#ifndef RAIL_CONTROL_CHASSIS_I_QUAD_CONTROL_HPP
+#define RAIL_CONTROL_CHASSIS_I_QUAD_CONTROL_HPP
+#include "quad_drive_data.hpp"
+
+namespace rail::chassis
+{
+    /**
+     * Non-owning control of a quad drive. It is rare for a taproot command
+     * to directly manage a quad drive; however, for those who are not of the faint-hearted,
+     * this allows such
+     */
+    template<class SpeedData>
+    class IQuadControl
+    {
+        using WheelSpeeds = QuadDriveData<SpeedData>;
+
+        virtual void setTargetWheelSpeeds(const WheelSpeeds& wheelSpeeds) = 0;
+        virtual WheelSpeeds getTargetWheelSpeeds() const = 0;
+    };
+}
+#endif
