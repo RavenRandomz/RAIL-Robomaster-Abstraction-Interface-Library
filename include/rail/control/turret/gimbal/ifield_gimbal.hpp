585042270ed74b0d5e4beb002847649599a2a060
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 16:38:29 -0500

    refactor(IFieldGimbal): Remove control responsibility from class

--- include/rail/control/turret/gimbal/ifield_gimbal.hpp
+++ include/rail/control/turret/gimbal/ifield_gimbal.hpp
@@ -1,18 +1,15 @@
 #ifndef RAIL_CONTROL_TURRET_I_FIELD_GIMBAL_HPP
 #define RAIL_CONTROL_TURRET_I_FIELD_GIMBAL_HPP
+#include "ifield_gimbal_control.hpp"
 namespace rail::control::turret
 {
     /**
      * Global, coordinate based controls for the gimbal
      */
     template <class AngularPosition>
-    class IFieldGimbal
+    class IFieldGimbal : virtual public IFieldGimbalControl<AngularPosition>
     {
-        virtual void setTargetFieldPitch(const AngularPosition& pitch) = 0;
-        virtual AngularPosition getTargetFieldPitch() const = 0;
-
-        virtual void setTargetFieldYaw(const AngularPosition& yaw) = 0;
-        virtual AngularPosition getTargetFieldYaw() const = 0;
+        virtual ~IFieldGimbal() = default;
     };
 }
 #endif
