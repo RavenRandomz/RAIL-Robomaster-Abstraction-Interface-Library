4a915a104126e283bc9a70f10bba04c9ee4550c0
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 17:23:35 -0500

    feat(IQuadDrive): Allow client owner to update() and initialize() instances

--- include/rail/chassis/iquad_drive.hpp
+++ include/rail/chassis/iquad_drive.hpp
@@ -1,6 +1,7 @@
 #ifndef RAIL_CONTROL_CHASSIS_I_QUAD_DRIVE_HPP
 #define RAIL_CONTROL_CHASSIS_I_QUAD_DRIVE_HPP
 #include "iquad_control.hpp"
+#include "rail/system/isystem.hpp"
 
 namespace rail::chassis
 {
@@ -9,7 +10,9 @@ namespace rail::chassis
      * Provides a uniform means of controlling 4 motors in a quad orientation.
      * This is particularly useful for the 3 main holonomic drives: omni, mecnaum, and swerve
      */
-    class IQuadDrive : virtual public IQuadControl<SpeedData>
+    class IQuadDrive:
+        virtual public IQuadControl<SpeedData>,
+        virtual public system::ISystem
     {
         using WheelSpeeds = IQuadControl<SpeedData>::WheelSpeeds;
         virtual ~IQuadDrive() = 0;
