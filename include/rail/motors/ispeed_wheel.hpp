c158a0fbdbc122255fe0ca1ba8db689603298459
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 16:03:15 -0500

    refactor(ISpeedWheel): Allow non-owning users to control ISpeedWheel instances

--- include/rail/motors/ispeed_wheel.hpp
+++ include/rail/motors/ispeed_wheel.hpp
@@ -1,16 +1,17 @@
 #ifndef RAIL_MOTORS_I_SPEED_WHEEL_H
 #define RAIL_MOTORS_I_SPEED_WHEEL_H
-#include "ispeed_motor.hpp"
+#include "irim_speed_control.hpp"
+#include "ispeed_control.hpp"
 
 namespace rail::motors
 {
+	/**
+	 * This is useful for precise control of chassis wheels and ammo booster flywheels.
+	 */
 	template <typename SpeedData, typename AngularSpeedData>
-	class ISpeedWheel : virtual public ISpeedMotor<AngularSpeedData>
+	class ISpeedWheel : virtual public IRimSpeedControl<SpeedData>, virtual public ISpeedControl<AngularSpeedData>
 	{
 	public:
-		virtual void setRimSpeed(const SpeedData& rimSpeed) = 0;
-		virtual SpeedData getRimSpeed() const = 0;
-
 		virtual ~ISpeedWheel() = default;
 	};
 }
