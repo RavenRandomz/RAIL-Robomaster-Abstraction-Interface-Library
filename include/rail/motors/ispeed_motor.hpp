6ebd9e04a3786d218b7a27437d68e2361cde4c09
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 15:42:34 -0500

    refactor(ISpeedMotor): Separate responsibility of updating motor (ownership) and controlling the motor(client)

--- include/rail/motors/ispeed_motor.hpp
+++ include/rail/motors/ispeed_motor.hpp
@@ -1,14 +1,13 @@
 #ifndef RAIL_MOTORS_I_SPEED_MOTOR_H
 #define RAIL_MOTORS_I_SPEED_MOTOR_H
+#include "ispeed_control.hpp"
+
 namespace rail::motors
 {
 	template <typename SpeedData>
-	class ISpeedMotor
+	class ISpeedMotor : public ISpeedControl<SpeedData>
 	{
 	public:
-		virtual void setTargetSpeed(const SpeedData& speed) = 0;
-		virtual SpeedData getTargetSpeed() const = 0;
-
 		virtual ~ISpeedMotor() = default;
 	};
 }
