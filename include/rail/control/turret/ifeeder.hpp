72a2c55509c9a5e967b80e3dab22361c6fe0b265
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 16:23:09 -0500

    refactor(IFeeder): Allow owning classes to control IFeeder

--- include/rail/control/turret/ifeeder.hpp
+++ include/rail/control/turret/ifeeder.hpp
@@ -1,19 +1,15 @@
 #ifndef RAIL_CONTROL_TURRET_I_FEEDER_HPP
 #define RAIL_CONTROL_TURRET_I_FEEDER_HPP
+#include "ifeeder_control.hpp"
+
 namespace rail::control::turret
 {
     /**
-     * Feeder/agitator interface. Allows external manual unjamming as well.
-     * (I understand some codebases automatically detect jams and immeidately
-     * reverse the motor) 
+     * Owning class for general feeders
      */
-    class IFeeder
+    class IFeeder : virtual public IFeederControl
     {
-        virtual void feedOn() = 0;    
-        virtual void feedOff() = 0;
-        virtual void unjamOn() = 0;
-        virtual void unjamOff() = 0;
-
+        virtual ~IFeeder() = default;
     };
 }
 #endif
