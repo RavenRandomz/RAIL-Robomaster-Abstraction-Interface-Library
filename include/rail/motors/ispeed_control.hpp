098577ddc59589797181417439c14f55ecbcfe19
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 15:38:57 -0500

    feat(ISpeedControl): Separate interface responsibility

---
+++ include/rail/motors/ispeed_control.hpp
@@ -0,0 +1,15 @@
+#ifndef RAIL_MOTORS_I_SPEED_CONTROL_HPP
+#define RAIL_MOTORS_I_SPEED_CONTROL_HPP
+namespace rail::motors
+{
+	template <typename SpeedData>
+	class ISpeedControl
+	{
+	public:
+		virtual void setTargetSpeed(const SpeedData& speed) = 0;
+		virtual SpeedData getTargetSpeed() const = 0;
+
+		virtual ~ISpeedControl() = default;
+	};
+}
+#endif
