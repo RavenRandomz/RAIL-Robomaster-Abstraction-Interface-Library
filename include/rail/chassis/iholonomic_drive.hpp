6a14bf73518bca9b27e74c4fac3f7da3c99b4e1d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 17:16:46 -0500

    fix(IHolonomicDrive): Prevent memory leaks and whatnot

--- include/rail/chassis/iholonomic_drive.hpp
+++ include/rail/chassis/iholonomic_drive.hpp
@@ -17,6 +17,7 @@ namespace rail::chassis
         virtual public IHolonomicControl<Velocity, AngularVelocity>,
         virtual public ISystem
     {
+        virtual ~IHolonomicDrive() = default;
     };
 }
 #endif
