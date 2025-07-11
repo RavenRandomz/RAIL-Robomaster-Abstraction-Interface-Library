241d3fcf87551f5498ade8f5397359200918c40d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 16:21:07 -0500

    feat(IFeederControl): Allow client to control IFeeder without owning it
    Allows commands to access what they only need

---
+++ include/rail/control/turret/ifeeder_control.hpp
@@ -0,0 +1,19 @@
+#ifndef RAIL_CONTROL_TURRET_I_FEEDER_CONTROL_HPP
+#define RAIL_CONTROL_TURRET_I_FEEDER_CONTROL_HPP
+namespace rail::control::turret
+{
+    /**
+     * Feeder/agitator interface. Allows external manual unjamming as well.
+     * (I understand some codebases automatically detect jams and immeidately
+     * reverse the motor).
+     * 
+     */
+    class IFeederControl
+    {
+        virtual void feedOn() = 0;    
+        virtual void feedOff() = 0;
+        virtual void unjamOn() = 0;
+        virtual void unjamOff() = 0;
+    };
+}
+#endif
