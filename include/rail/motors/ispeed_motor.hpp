5c60fb9557bbe5431ad7b996b1d13794e77963d9
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 15:50:05 -0500

    feat(ISpeedMotor): Allow owner of instance to regularily update motor

--- include/rail/motors/ispeed_motor.hpp
+++ include/rail/motors/ispeed_motor.hpp
@@ -1,11 +1,12 @@
 #ifndef RAIL_MOTORS_I_SPEED_MOTOR_H
 #define RAIL_MOTORS_I_SPEED_MOTOR_H
 #include "ispeed_control.hpp"
+#include "rail/systems/isystem.hpp"
 
 namespace rail::motors
 {
 	template <typename SpeedData>
-	class ISpeedMotor : public ISpeedControl<SpeedData>
+	class ISpeedMotor : public ISpeedControl<SpeedData>, public systems::ISystem
 	{
 	public:
 		virtual ~ISpeedMotor() = default;
