5380aa17b461e79577109a7e83f5e9c671a1ab83
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 16:19:39 -0500

    feat(IAmmoBooster): Allow owning client to regularily update

--- include/rail/control/turret/iammo_booster.hpp
+++ include/rail/control/turret/iammo_booster.hpp
@@ -1,10 +1,11 @@
 #ifndef RAIL_CONTROL_TURRET_I_AMMO_BOOSTER_HPP
 #define RAIL_CONTROL_TURRET_I_AMMO_BOOSTER_HPP
 #include "iammo_booster_control.hpp"
+#include "rail/systems/isystem.hpp"
 
 namespace rail::control::turret
 {
-    class IAmmoBooster : virtual public IAmmoBoosterControl
+    class IAmmoBooster : virtual public IAmmoBoosterControl, virtual public systems::ISystem
     {
         virtual ~IAmmoBooster() = default;
     };
