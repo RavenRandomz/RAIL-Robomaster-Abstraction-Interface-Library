e7d0a4f4cf0205296896473738d9737180b8290b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 16:41:30 -0500

    feat(IFieldGimbal): Allow owner to initialize and update

--- include/rail/control/turret/gimbal/ifield_gimbal.hpp
+++ include/rail/control/turret/gimbal/ifield_gimbal.hpp
@@ -1,13 +1,14 @@
 #ifndef RAIL_CONTROL_TURRET_I_FIELD_GIMBAL_HPP
 #define RAIL_CONTROL_TURRET_I_FIELD_GIMBAL_HPP
 #include "ifield_gimbal_control.hpp"
+#include "rail/systems/isystem.hpp"
 namespace rail::control::turret
 {
     /**
      * Global, coordinate based controls for the gimbal
      */
     template <class AngularPosition>
-    class IFieldGimbal : virtual public IFieldGimbalControl<AngularPosition>
+    class IFieldGimbal : virtual public IFieldGimbalControl<AngularPosition>, virtual public systems::ISystem
     {
         virtual ~IFieldGimbal() = default;
     };
