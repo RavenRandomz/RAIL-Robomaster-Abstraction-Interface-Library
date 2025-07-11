a8a83f40e921649a5172605f3fddca8b38e7357e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-11 16:48:06 -0500

    feat(IFeeder): Allow owning class to initialize and update

--- include/rail/control/turret/feeder/ifeeder.hpp
+++ include/rail/control/turret/feeder/ifeeder.hpp
@@ -1,13 +1,13 @@
 #ifndef RAIL_CONTROL_TURRET_I_FEEDER_HPP
 #define RAIL_CONTROL_TURRET_I_FEEDER_HPP
 #include "ifeeder_control.hpp"
-
+#include "rail/system/isystem.hpp"
 namespace rail::control::turret
 {
     /**
      * Owning class for general feeders
      */
-    class IFeeder : virtual public IFeederControl
+    class IFeeder : virtual public IFeederControl, virtual public system::ISystem
     {
         virtual ~IFeeder() = default;
     };
