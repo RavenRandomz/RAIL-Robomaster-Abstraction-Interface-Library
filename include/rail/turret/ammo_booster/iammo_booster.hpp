ac1acea05a1d873c44ee638202d698be0af1d789
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 12:45:55 -0500

    chore(IAmmoBooster): Update to new IAmmoBooster api

--- include/rail/turret/ammo_booster/iammo_booster.hpp
+++ include/rail/turret/ammo_booster/iammo_booster.hpp
@@ -1,11 +1,15 @@
-#ifndef RAIL_CONTROL_TURRET_I_AMMO_BOOSTER_HPP
-#define RAIL_CONTROL_TURRET_I_AMMO_BOOSTER_HPP
+#ifndef RAIL_TURRET_I_AMMO_BOOSTER_HPP
+#define RAIL_TURRET_I_AMMO_BOOSTER_HPP
 #include "iammo_booster_control.hpp"
 #include "rail/system/isystem.hpp"
 
 namespace rail::turret
 {
-    class IAmmoBooster : virtual public IAmmoBoosterControl, virtual public system::ISystem
+    /**
+     * General abstraction for ammo booster subsystems
+     */
+    template <typename Velocity>
+    class IAmmoBooster : virtual public IAmmoBoosterControl<Velocity>, virtual public system::ISystem
     {
         virtual ~IAmmoBooster() = default;
     };
