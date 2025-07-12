e5551c353a56dfdecd4bdcf39fb6990e644bdb7d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 13:09:04 -0500

    test(IHolonomicDrive): Update to new inheritance structure

--- include/rail/mock/chassis/iholonomic_drive_mock.hpp
+++ include/rail/mock/chassis/iholonomic_drive_mock.hpp
@@ -1,7 +1,9 @@
-#ifndef RAIL_CONTROL_CHASSIS_I_HOLONOMIC_DRIVE_MOCK_HPP
-#define RAIL_CONTROL_CHASSIS_I_HOLONOMIC_DRIVE_MOCK_HPP
-#include "rail/chassis/iholonomic_drive.hpp"
-namespace rail::control::chassis
+#ifndef RAIL_CHASSIS_I_HOLONOMIC_DRIVE_MOCK_HPP
+#define RAIL_CHASSIS_I_HOLONOMIC_DRIVE_MOCK_HPP
+#include "iholonomic_control_mock.hpp"
+#include "rail/mock/system/isystem_mock.hpp"
+
+namespace rail::chassis
 {
     /**
      * A Holonomic Drive is essentially any 2d drive that can translate
@@ -12,13 +14,11 @@ namespace rail::control::chassis
      * x and y.
      */
     template <class Velocity, class AngularVelocity>
-    class IHolonomicDriveMock : public IHolonomicDrive<Velocity, AngularVelocity>
+    class IHolonomicDrive:
+        virtual public IHolonomicControlMock<Velocity, AngularVelocity>,
+        virtual public ISystemMock
     {
-        MOCK_METHOD(void, setTargetTranslation, (const Velocity& velocity), (override));
-        MOCK_METHOD(void, setTargetRotation, (const AngularVelocity& rotation), (override));
-
-        MOCK_METHOD(Velocity, getTargetTranslation, (), (const, override));
-        MOCK_METHOD(AngularVelocity, getTargetRotation, (), (const, override));
+        virtual ~IHolonomicDrive() = default;
     };
 }
 #endif
