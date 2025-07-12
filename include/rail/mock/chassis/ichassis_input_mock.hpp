b2c63277e98a68275f363ce07b2694a91c20f543
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 13:28:25 -0500

    test(IChassisInputMock): Allow general testing

---
+++ include/rail/mock/chassis/ichassis_input_mock.hpp
@@ -0,0 +1,21 @@
+#ifndef RAIL_MOCK_CHASSIS_I_CHASSIS_INPUT_MOCK_HPP
+#define RAIL_MOCK_CHASSIS_I_CHASSIS_INPUT_MOCK_HPP
+#include "rail/chassis/ichassis_input.hpp"
+#include <gmock/gmock.h>
+
+namespace rail::chassis
+{
+    /**
+     * This is for the abstraction for use inputs.
+     * It is recommended for these inputs to be floats or doubles
+     * which then converted into specific speeds by the command
+     * in order to have single responsibility
+     */
+    template <typename Translation, typename Rotation>
+    class IChassisInputMock
+    {
+        MOCK_METHOD(Translation, getTranslation, (), (const, override)); 
+        MOCK_METHOD(Rotation, getRotation, (), (const, override));
+    };
+}
+#endif
