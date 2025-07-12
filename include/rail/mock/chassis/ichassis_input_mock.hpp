24e6cec92a8c422d31b27a47f9282c7570df5720
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 14:24:57 -0500

    test(ChassisInputReader): Update to refactored paradigm

--- include/rail/mock/chassis/ichassis_input_mock.hpp
+++ include/rail/mock/chassis/ichassis_input_mock.hpp
@@ -1,6 +1,7 @@
 #ifndef RAIL_MOCK_CHASSIS_I_CHASSIS_INPUT_MOCK_HPP
 #define RAIL_MOCK_CHASSIS_I_CHASSIS_INPUT_MOCK_HPP
 #include "rail/chassis/ichassis_input.hpp"
+#include "ichassis_input_reader_mock.hpp"
 #include <gmock/gmock.h>
 
 namespace rail::chassis
@@ -10,13 +11,14 @@ namespace rail::chassis
      * It is recommended for these inputs to be floats or doubles
      * which then converted into specific speeds by the command
      * in order to have single responsibility
+     * 
+     * This is a non-owning interface
      */
     template <typename Translation, typename Rotation>
-    class IChassisInputMock
+    class IChassisInputMock:
+        virtual public IChassisInput<Translation, Rotation>
+        virtual public IChassisInputReaderMock<Translation, Rotation>,
     {
-    public:
-        MOCK_METHOD(Translation, getTranslation, (), (const, override)); 
-        MOCK_METHOD(Rotation, getRotation, (), (const, override));
     };
 }
 #endif
