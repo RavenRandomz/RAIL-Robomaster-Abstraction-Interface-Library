b188965bda7b7888ef4f403990be6cede2acf552
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 12:42:10 -0500

    feat(ISimpleAmmoBooster): Allow owning classes to access simple ammo booster

---
+++ include/rail/turret/ammo_booster/isimple_ammo_booster.hpp
@@ -0,0 +1,28 @@
+#ifndef RAIL_TURRET_I_SIMPLE_AMMO_BOOSTER_HPP
+#define RAIL_TURRET_I_SIMPLE_AMMO_BOOSTER_HPP
+#include "isimple_ammo_booster_control.hpp"
+#include "rail/system/isystem.hpp"
+
+namespace rail::turret
+{
+    /**
+     * This is created for backward compatibility with the fang-mcb
+     * 2025 design. I have realized that it is better for the commands
+     * to have control of the boosting functionality as it allows
+     * various commands to have different speeds.
+     * 
+     * The point of a subsystem is to abstract a given component while a command
+     * imbues it with a behavior. Boosting is a behavior while the
+     * abstraction should have been setTargetBoostVelocity().
+     * 
+     * In order to allow for the refactoring process to go more smoothly,
+     * this class allows for a transition into dependency injection without
+     * requiring an overhaul of where the boost velocity configuration is stored
+     * and whatnot
+     */
+    class ISimpleAmmoBooster : virtual public ISimpleAmmoBoosterControl, virtual public system::ISystem
+    {
+        virtual ~ISimpleAmmoBooster() = default;
+    };
+};
+#endif
