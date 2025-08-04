f2967b79592e414635a2754ea8dc73489436c517
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-04 10:13:18 -0500

    feat(gimbal): Allow a gimbal to be constructed from separate yaw system

---
+++ include/rail/turret/gimbal/ifield_yaw_system.hpp
@@ -0,0 +1,21 @@
+#ifndef RAIL_TURRET_GIMBAL_I_FIELD_YAW_SYSTEM_HPP
+#define RAIL_TURRET_GIMBAL_I_FIELD_YAW_SYSTEM_HPP
+#include "ifield_yaw_control.hpp"
+#include "rail/system/isystem.hpp"
+namespace rail::turret
+{
+    /**
+     * This allows a gimbal to be constructed from a separate pitch and
+     * yaw system. This is desireable because the pitch and yaw turret
+     * can achieve global coordinates in many ways - with imus or dead
+     * reckoning.
+     */
+    template <typename AngularPosition>
+    class IFieldYawSystem:
+        virtual public IFieldYawControl<AngularPosition>,
+        virtual public system::ISystem
+    {
+        virtual ~IFieldYawSystem() {};
+    };
+}
+#endif
