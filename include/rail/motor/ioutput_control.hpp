5c6d938418e9b41a407d166d3ce72cb575f74362
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-07 12:06:33 -0500

    feat(OutputControl): Add ability to control motors via output

---
+++ include/rail/motor/ioutput_control.hpp
@@ -0,0 +1,21 @@
+#ifndef RAIL_MOTOR_I_OUTPUT_MOTOR_HPP
+#define RAIL_MOTOR_I_OUTPUT_MOTOR_HPP
+namespace rail::motor
+{	
+    /**
+     * When controlling a motor whose output (abstract value corresponding either to
+     * current or voltage) can be controlled.
+     * 
+     * Useful for interfacing with generic PID programs.
+	 */
+	template <typename OutputData>
+	class IOutputControl 
+	{
+	public:
+		virtual void setTargetOutput(const OutputData& output) = 0;
+
+		virtual ~IOutputControl() {};
+	};
+
+}
+#endif
