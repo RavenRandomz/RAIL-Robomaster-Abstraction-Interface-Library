ced5f2c69865cfa6f063d361151b5d70ab91bced
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-02-14 13:47:33 -0600

    feat: add testing utility

---
+++ include/rail/mock/turret/gimbal/igimbal_input_handler_mock.hpp
@@ -0,0 +1,18 @@
+#ifndef RAIL_TURRET_I_GIMBAL_INPUT_HANDLER_MOCK_HPP
+#define RAIL_TURRET_I_GIMBAL_INPUT_HANDLER_MOCK_HPP
+#include "rail/turret/igimbal_input_handler.hpp"
+#include <gmock/gmock.h>
+
+namespace rail
+{
+    template<typename Data>
+    class IGimbalInputHandlerMock : public ITurretInputHandler
+    {
+    public:
+        MOCK_METHOD(Data, getPitch, (), (const, override));
+        MOCK_METHOD(Data, getYaw,, (), (const, override));
+
+        virtual ~IGimbalInputHandlerMock() {}
+    };
+}
+#endif
